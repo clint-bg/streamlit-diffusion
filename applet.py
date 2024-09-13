4b35a3d94ac3082b83658073159125b75b914645
Clint Guymon <clintguymon@Clints-Air.app.byu.edu>
2024-09-13 17:13:29 -0600

    start

--- applet.py
+++ applet.py
@@ -28,13 +28,14 @@ for i in range(nwalks):
 
 
 #plot one of the simulation runs (the last one)
-plt.plot(x_loc,z_loc)
-plt.plot(x_loc[0],z_loc[0],'go')
-plt.plot(x_loc[-1],z_loc[-1],'ro')
-plt.xlabel('x position'); plt.ylabel('z position')
-plt.title('3-D Random Walk plotted in 2D')
-plt.legend(['Random Walk','Start','End'])
-plt.show()
+fig, ax = plt.subplots()
+ax.plot(x_loc,z_loc)
+ax.plot(x_loc[0],z_loc[0],'go')
+ax.plot(x_loc[-1],z_loc[-1],'ro')
+ax.xlabel('x position'); plt.ylabel('z position')
+ax.title('3-D Random Walk plotted in 2D')
+ax.legend(['Random Walk','Start','End'])
+st.pyplot(fig)
 
 dicta = {'Number of steps''Average squared distance': np.mean(sqdists,axis=0)}
 df = pd.DataFrame(dicta)
