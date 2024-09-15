653b268050546783367a59121184e9be73c597f1
Clint Guymon <clintguymon@Clints-Air.lan>
2024-09-14 22:42:05 -0600

    start

--- applet.py
+++ applet.py
@@ -33,13 +33,7 @@ for i in range(nwalks):
 #plot one of the simulation runs (the last one)
 fig, ax = plt.subplots()
 #format the text to a smaller size
-fig.update_layout(
-    font=dict(
-        family="Courier New, monospace",
-        size=8,
-        color="#7f7f7f"
-    )
-)
+plt.rc('font', size=8)
 ax.plot(x_loc,z_loc)
 ax.plot(x_loc[0],z_loc[0],'go')
 ax.plot(x_loc[-1],z_loc[-1],'ro')
